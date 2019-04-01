3895f5a125cffe6f55e434ac30fc92cd19316b7a
Robin Lebhar <robin_lebhar@hotmail.fr>
2019-04-01 18:37:41 -0400

    Add files via upload

---
+++ src/components/rate-exchange-list-item.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ReactChartKick, { AreaChart } from "react-chartkick";
+import Chart from "chart.js";
+
+ReactChartKick.addAdapter(Chart);
+class RateEchangeListItem extends Component {
+  render() {
+    console.log(this.props.rateExchange);
+    const { flag, name, rates, currencyCode } = this.props.rateExchange;
+    return (
+      <tr>
+        <td>{name}<br /><img src={flag} height="60" width="100" /></td>
+        <td className="col-md-12">
+          <AreaChart
+            data={formatData(rates, currencyCode)}
+            xtitle="Date"
+            ytitle={currencyCode}
+          />
+        </td>
+      </tr>
+    );
+  }
+}
+
+function formatData(rates, currencyCode) {
+  return Object.keys(rates).map(date => {
+    return [date, rates[date][currencyCode]];
+  });
+}
+
+export default RateEchangeListItem;
